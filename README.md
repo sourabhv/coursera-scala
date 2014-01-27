bf04692811a7e2c7fea0a3966fe5aca2994e23a1
Sourabh Verma <sourabh.coder@gmail.com>
2014-01-27 10:12:54 +0530

    Adds week1 - recfun

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-#[Principles of Functional Programming in Scala](https://class.coursera.org/progfun-003)
+##[Principles of Functional Programming in Scala](https://class.coursera.org/progfun-003)
---
+++ week1/recfun/src/main/scala/recfun/Main.scala
@@ -0,0 +1,44 @@
+package recfun
+import common._
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int = {
+    if (c == r || r == 0 || c == 0) 1
+    else pascal(c-1, r-1) + pascal(c, r-1)
+  }
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = {
+    def loop(c: Int, s:List[Char]): Boolean = {
+      if(s.isEmpty) c == 0
+      else if (c < 0) false
+      else if (s.head == '(') loop(c+1, s.tail)
+      else if (s.head == ')') loop(c-1, s.tail)
+      else loop(c, s.tail)
+    }
+    loop(0, chars)
+  }
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0) 1
+    else if (coins.isEmpty || money < 0) 0
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+  }
+}
